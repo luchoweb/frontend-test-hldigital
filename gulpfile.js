03211a4d5c90ed597dcafd9f11cf92e68641c625
Lucho Web <luchoweb.dev@gmail.com>
2024-03-27 15:52:16 -0500

    header and intro section progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const cleanCss = require('gulp-clean-css');
 
 function compileSass() {
   return gulp
     .src("css/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCss())
     .pipe(gulp.dest("css"));
 }
 gulp.task("sass", compileSass);
