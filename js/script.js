89c0a95b554204542207fd54f020c9f597045046
Lucho Web <luchoweb.dev@gmail.com>
2024-03-28 09:40:01 -0500

    slider progress

--- js/script.js
+++ js/script.js
@@ -9,8 +9,26 @@ const closeHeaderBar = () => {
   });
 };
 
+const changeSlide = () => {
+  document.body.onclick = function (ev) {
+    const $this = ev.target;
+
+    if ($this.getAttribute("class") == "slide-dot") {
+      const slideIndex = $this.getAttribute("data-slide");
+      const elements = {
+        currentSlide: document.querySelector(".slide.current"),
+        nextSlide: document.querySelector(`.slide[data-slide="${slideIndex}"]`)
+      };
+
+      elements.currentSlide.classList.remove("current");
+      elements.nextSlide.classList.add("current");
+    }
+  };
+};
+
 const onInit = () => {
   closeHeaderBar();
+  changeSlide();
 };
 
 window.onload = onInit;
