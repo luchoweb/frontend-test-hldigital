3728573207d8e1b8cf8c52b67ad31774d06ae8d3
Lucho Web <luchoweb.dev@gmail.com>
2024-03-28 21:07:05 -0500

    landing page done and gulp minifiers setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const cleanCss = require('gulp-clean-css');
+const cleanCss = require("gulp-clean-css");
+const terser = require("gulp-terser");
 
-function compileSass() {
+function compileCSS() {
   return gulp
     .src("css/scss/*.scss")
     .pipe(sass().on("error", sass.logError))
-    //.pipe(cleanCss()) // Minify
+    .pipe(cleanCss())
     .pipe(gulp.dest("css"));
 }
-gulp.task("sass", compileSass);
+gulp.task("css", compileCSS);
 
-function watchSass() {
-  gulp.watch("css/scss/**/*.scss", compileSass);
+function compileJS() {
+  return gulp.src("js/scripts/*.js")
+    .pipe(terser())
+    .pipe(gulp.dest("js"));
 }
-gulp.task("watch", watchSass);
+gulp.task("js", compileJS);
+
+function watch() {
+  gulp.watch(["css/scss/**/*.scss", "js/scripts/*.js"], gulp.series('css', 'js'));
+}
+gulp.task("watch", watch);
